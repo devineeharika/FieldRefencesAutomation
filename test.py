2f1fc48db858915f535a9b3bad9a11f6423d7c7c
devineeharika <40334955+devineeharika@users.noreply.github.com>
2024-12-20 11:16:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
